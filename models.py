6fb410a2899bfe58d5a22acea29aa58f1751a7b1
Anton Komarov <anton_komarov_qa@ukr.net>
2023-11-07 12:02:21 +0200

    Add slug_title field to Book model

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Date, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
+from slugify.slugify import slugify
 from db.database import Base
 
 
@@ -8,8 +9,14 @@ class Author(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(255), unique=True)
+    slug_name = Column(String(511))
 
     books = relationship("Book", back_populates="author")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.slug_name = slugify(self.name) # type: ignore
+
 
 
 class Genre(Base):
@@ -29,6 +36,12 @@ class Book(Base):
     author_id = Column(Integer, ForeignKey("authors.id"))
     genre_id = Column(Integer, ForeignKey("genres.id"))
     date_published = Column(Date)
+    slug_title = Column(String(511))
 
     author = relationship("Author", back_populates="books")
     genre = relationship("Genre", back_populates="books")
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.slug_title = slugify(self.title) # type: ignore
